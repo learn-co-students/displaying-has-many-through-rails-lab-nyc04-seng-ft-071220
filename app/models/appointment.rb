9b2a4e5f96ab2bf9011f2418dcb8cfcd6d6c7183
iannakim <dear.hyunji@gmail.com>
2020-09-03 10:02:32 -0400

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,11 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  default_scope { order(appointment_datetime: :desc)}
+
+  def datetime
+  self.appointment_datetime.to_formatted_s(:long)
+  end
+  
 end
